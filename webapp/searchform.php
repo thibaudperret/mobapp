46d9489a2ae748fea55a6d446437b15e35f0c5a4
thibaudperret <thibaud.perret@epfl.ch>
2017-02-04 22:37:30 +0100

    added a logo to home page

---
+++ webapp/searchform.php
@@ -0,0 +1,151 @@
+<html>
+<head>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+	<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+	
+	<style>
+		.ui-my-fixed {
+			position: fixed;
+			top: 0;
+			width: 100%;
+			background-color: #f9f9f9;
+		}
+		
+		.ui-my-free {
+			padding: 20px;
+			margin-top:3 0px;
+			height: 1500px;
+		}
+	</style>
+	
+	<title>Jobino</title>
+
+</head>
+
+<body>
+
+	<div data-role="page" id="home">
+		<div data-role="main" class="ui-content">
+			<div class="ui-content ui-my-fixed">
+				<h1>Search for</h1>
+			</div>
+			
+			<div class="ui-content ui-my-free">
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+				<p>Test</p><br>
+			</div>
+		</div>
+
+		<div data-role="footer" data-position="fixed" data-tap-toggle="false">
+			<div data-role="navbar">
+				<ul>
+					<li><a href="search.html"   rel="external" data-icon="search"></a></li>
+					<li><a href="add.html"      rel="external" data-icon="plus"></a></li>
+					<li><a href="messages.html" rel="external" data-icon="mail"></a></li>
+					<li><a href="profile.html"  rel="external" data-icon="user"></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
